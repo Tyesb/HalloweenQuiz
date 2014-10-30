3334dda4e92e391149b423ccf98fdceed101a4ab
cheetah <cheetah@devacademy.co.nz>
2014-10-31 12:28:27 +1300

    Built GameInfoModel

---
+++ HalloweenQuiz/HalloweenQuiz/LoaderController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HalloweenQuiz
+{
+
+    class LoaderController
+    {
+        string question;
+        string correctAnswer;
+        string[] guesses;
+
+        private List<AQuestion> questions; 
+        public string[] Load(string file)
+       
+        {
+            using (StreamReader reader = new StreamReader(file))
+            {
+                
+                QuestionModel currentQuestionModel = new QuestionModel(question, correctAnswer, guesses);
+            }
+
+
+        }
+    }
+}
---
+++ HalloweenQuiz/HalloweenQuiz/QuestionModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloweenQuiz
+{
+    public class QuestionModel
+    {
+        public string CorrectAnswer;
+        public string Question;
+        public Dictionary<int, string> answerList;
+ 
+        public QuestionModel(string correctanswer, string question, string[]guesses)
+        {
+            CorrectAnswer = correctanswer;
+            Question = question;
+            answerList.AddRange(guesses);
+        }
+
+
+    }
+}
